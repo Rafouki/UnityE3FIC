41c85709fcd175b99f8b43472328010605d987fc
Alexis <alexisd1999@live.fr>
2020-05-13 21:51:53 +0200

    test push

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    Vector3 offsetCamera;
+
+    [Range(0.01f, 1.0f)]
+    [SerializeField] float smooth;
+    public float MinDist, CurrentDist, MaxDist, TranslateSpeed, AngleH, AngleV;
+    void Start()
+    {
+        //offsetCamera = transform.position - target.position;
+    }
+
+    void Update()
+    {
+
+        //transform.position = Vector3.Lerp(transform.position, target.position, 0.1f);
+        //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        /*
+        Vector3 cameraPosition = target.position + offsetCamera;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, cameraPosition, smooth);
+        transform.position = smoothPosition;
+        transform.position = cameraPosition;
+        transform.LookAt(target);
+        */
+        //Vector3 cameraPosition = target.position + offsetCamera;
+        //float Hcamera = Input.GetAxis("R_Horizontal");
+        //Debug.Log("Value axis : " + Hcamera);
+        //transform.position = new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+
+        //transform.RotateAround(target.position,Vector3.up, Hcamera*30*Time.deltaTime );
+        //transform.LookAt(target);
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Translation & Rotation")]
+    [Tooltip("La vitesse de transaltion en m.s-1")]
+    [SerializeField] float m_TranslationSpeed;
+    [SerializeField] float m_Speed;
+
+    [Tooltip("La vitesse de rotation en °.s-1")]
+    [SerializeField] float m_RotationSpeed;
+    [SerializeField] float m_UprightRotKLerp;
+
+
+    Rigidbody m_Rigidbody;
+    
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        m_Rigidbody.AddForce(movement * m_Speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        m_Rigidbody.AddForce(-movement/2 * m_Speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        
+
+
+        /*
+        //dynamique cinétique
+        float vInput = Input.GetAxis("Vertical");
+        float hInput = Input.GetAxis("Horizontal");
+
+
+        //Mouvement
+        Vector3 translationVect = vInput * transform.forward * m_TranslationSpeed * Time.deltaTime;
+        m_Rigidbody.MovePosition(transform.position + translationVect);
+
+        //qRotation*qOrientation = qNewOrientation
+        float deltaAngle = hInput * m_RotationSpeed * Time.fixedDeltaTime;
+        Quaternion qRot = Quaternion.AngleAxis(deltaAngle, transform.up);
+        //Quaternion qUpright = Quaternion.FromToRotation(transform.up, Vector3.up);
+        //Quaternion newOrientation = Quaternion.Lerp(transform.rotation, qUpright * transform.rotation, m_UprightRotKLerp * Time.fixedDeltaTime);
+        Quaternion newOrientation = Quaternion.Lerp(transform.rotation, qRot * transform.rotation, m_UprightRotKLerp * Time.fixedDeltaTime);
+        newOrientation = qRot * newOrientation;
+        m_Rigidbody.MoveRotation(newOrientation);
+        */
+        
+    }
+
+
+
+}
