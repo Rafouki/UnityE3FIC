021a9484b2adee9b93db3b613d544ea381f62fe7
Alexis <alexisd1999@live.fr>
2020-05-13 22:56:15 +0200

    rage quit camera

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -9,31 +9,31 @@ public class CameraController : MonoBehaviour
 
     [Range(0.01f, 1.0f)]
     [SerializeField] float smooth;
-    public float MinDist, CurrentDist, MaxDist, TranslateSpeed, AngleH, AngleV;
     void Start()
     {
-        //offsetCamera = transform.position - target.position;
+        offsetCamera = transform.position - target.position;
     }
 
     void Update()
     {
 
-        //transform.position = Vector3.Lerp(transform.position, target.position, 0.1f);
-        //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
-        /*
+
+        //transform.Rotate(0, transform.rotation.y, 0);
+
+        
         Vector3 cameraPosition = target.position + offsetCamera;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, cameraPosition, smooth);
-        transform.position = smoothPosition;
-        transform.position = cameraPosition;
-        transform.LookAt(target);
-        */
-        //Vector3 cameraPosition = target.position + offsetCamera;
-        //float Hcamera = Input.GetAxis("R_Horizontal");
-        //Debug.Log("Value axis : " + Hcamera);
-        //transform.position = new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z);
-
-        //transform.RotateAround(target.position,Vector3.up, Hcamera*30*Time.deltaTime );
+        //transform.position = smoothPosition;
         //transform.LookAt(target);
+        
+        float Hcamera = Input.GetAxis("R_Horizontal");
+        Debug.Log("Value axis : " + Hcamera);
+        transform.position = new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+        transform.RotateAround(target.position,Vector3.up, Hcamera*30*Time.deltaTime );
+        Debug.Log(target.position.ToString());
+        
+        transform.LookAt(target);
+
 
     }
 }
