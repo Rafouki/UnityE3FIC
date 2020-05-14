d729bebe9fe56ae7a8040a6ecd74fecbb4e96d9a
Rafouki <58076280+Rafouki@users.noreply.github.com>
2020-05-14 09:52:23 +0200

    Fix camera : follow movement of character

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    Transform targetCamera;
+    Vector3 to;
+    Transform targetCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetCamera = GameObject.Find("Target").transform;
+        targetCharacter = GameObject.Find("Sphere").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,targetCamera.position, 1);
+        to = targetCharacter.transform.position - transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(to),1);
+    }
+}
